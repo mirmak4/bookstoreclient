a71910ef337f45f3bf19a1f4c08a3f34698f9342
Miron Maksymiuk <miron.maksymiuk@globallogic.com>
2023-06-22 12:06:14 +0200

    Added basic app layout, with appbar. Added main Layout component, which will render all app components inside, and Header component with appbar.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 import baseUrl from '../config';
+import Layout from './layout/layout';
 
 const App = () => {
 
     axios(`${baseUrl}/bookstore/api/v1/books`).then(books => {
         console.log(books);
     });
-    return <div>My first component</div>;
+    return (
+    
+        <Layout>
+            <div>My first component</div>
+        </Layout>
+    );
 }
 
 export default App;
