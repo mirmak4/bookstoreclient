33ac671a91fc35ee0f1b20434bc36a6cb380673e
Miron Maksymiuk <miron.maksymiuk@globallogic.com>
2023-06-23 15:24:48 +0200

    Added BookSelector for reading data from redux store, specifically books list, and saving it into BookContainer.

--- src/components/book/BookContainer.jsx
+++ src/components/book/BookContainer.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import getBooksAction from '../../modules/book/BookAction';
 import BookFiler from './BookFilter';
 import styles from './BookStyles.module.css';
+import { getBooksSelector } from '../../modules/book/BookSelector';
 
 const BookContainer = () => {
 
     const dispatch = useDispatch();
-    dispatch(getBooksAction());
+    useEffect(() => {
+        dispatch(getBooksAction());
+    }, [dispatch]);
+    const books = useSelector(getBooksSelector);
+    console.log(books);
     
     return (
         <Box className={styles.bookContainer}>
