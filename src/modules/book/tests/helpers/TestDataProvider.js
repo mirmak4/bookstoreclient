5e857a4ca07706c723baa7360eb02c92ef168f24
Miron Maksymiuk <miron.maksymiuk@globallogic.com>
2023-06-23 17:08:05 +0200

    Added unit test for BookReducer.

--- src/modules/book/tests/helpers/TestDataProvider.js
+++ src/modules/book/tests/helpers/TestDataProvider.js
@@ -12,9 +12,21 @@ const getBooksActionData = [
 const getBooksActionType = 'BOOKS_RESPONSE';
 // GET BOOKS ACTION END
 
+// BOOK REDUCER START
+const initialBookReducerState = {
+    books: []
+};
+
+const getBooksReducerData = {
+    books: getBooksActionData
+};
+// BOOK REDUCER END
+
 const testData = {
     getBooksActionData: getBooksActionData,
-    getBooksActionType: 'BOOKS_RESPONSE'
+    getBooksActionType: 'BOOKS_RESPONSE',
+    initialBookReducerState: initialBookReducerState,
+    getBooksReducerData: getBooksReducerData
 };
 
 export default testData;
