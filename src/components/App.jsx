2b418fe8fa170e6e7486409c20fcba2f59b37d4c
Miron Maksymiuk <miron.maksymiuk@globallogic.com>
2023-06-30 18:08:24 +0200

    Added router setup with browser router, routes and and single route with paths for book container and login components. Added empty login component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Layout from './layout/layout';
 import BookContainer from './book/BookContainer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './user/Login';
 
+// Routes is enabling just one Route at a time
 const App = () => {
 
     return (
-        <Layout>
-            <BookContainer />
-        </Layout>
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route path='/login' element={ <Login /> } />
+                    
+                    <Route exact path='/' element={ <BookContainer /> } />
+                </Routes>
+            </Layout>
+        </Router>
     );
 }
 
